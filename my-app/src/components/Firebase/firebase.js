22d04d38b2e0a5aa792c89d09aebca86958d2b46
qccanada2013 <1145940288@qq.com>
2019-09-04 09:35:28 -0400

    change firebase config to the common one.

--- my-app/src/components/Firebase/firebase.js
+++ my-app/src/components/Firebase/firebase.js
@@ -2,12 +2,13 @@ import app from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
 const config = {
-    apiKey: "AIzaSyB0wGmakoiS3_vAR3PNGVTFmNw-VNhbV10",
-    authDomain: "cs8803-d077d.firebaseapp.com",
-    databaseURL: "https://cs8803-d077d.firebaseio.com",
-    projectId: "cs8803-d077d",
+    apiKey: "AIzaSyA_ow4mw0HoRmrlhqeMMBUC-LocdyQznzc",
+    authDomain: "myfitness-696c2.firebaseapp.com",
+    databaseURL: "https://myfitness-696c2.firebaseio.com",
+    projectId: "myfitness-696c2",
     storageBucket: "",
-    messagingSenderId: "626445668972",
+    messagingSenderId: "680315578242",
+    appId: "1:680315578242:web:19be07bd9fbe719f"
   };
 
   class Firebase {
